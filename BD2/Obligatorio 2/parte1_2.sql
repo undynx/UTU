5838c0eda5efeb4abfc3ddc621fc190407318c88
Maité <68931661+undynx@users.noreply.github.com>
2023-06-16 19:09:54 -0300

    Update parte1_2.sql

--- BD2/Obligatorio 2/parte1_2.sql
+++ BD2/Obligatorio 2/parte1_2.sql
@@ -1,22 +1,21 @@
-/* TO DO: 
-	* Preguntarle al profe por qué al disparar una signal no se inserta en la tabla inconsist_r3
-    * Hacer el trigger para cuando se hace un update
-    * https://dev.mysql.com/doc/refman/8.0/en/signal.html#signal-effects
-*/
-
 CREATE TABLE inconsist_r3(
 	id_sucursal int,
     id_personal int
 );
 
-drop trigger r3_encargados
 delimiter //
-CREATE TRIGGER r3_encargados AFTER INSERT on sucursales
+CREATE PROCEDURE insertInconsistencia(in id_sucursal int, in id_personal int)
+BEGIN
+	insert into inconsist_r3 VALUES(id_sucursal, id_personal);
+END;
+//
+
+delimiter //
+CREATE TRIGGER r3_encargados_AI AFTER INSERT on sucursales
 for each row
 begin
 	DECLARE idPersonalExists int default(null);
     DECLARE errorMessage varchar(255);
-    DECLARE errorSignal boolean default(false);
     
 	IF NEW.idEncargado IS NOT NULL THEN
 		SELECT idPersonal INTO idPersonalExists
@@ -25,19 +24,30 @@ begin
 			and activo = true;
             
 		IF idPersonalExists IS NULL THEN
-			INSERT INTO inconsist_r3 values(NEW.idSucursal, NEW.idEncargado);
-            SET errorSignal = true;
-		END IF;
-        
-        IF errorSignal = true THEN
+			CALL insertInconsistencia(NEW.idSucursal, NEW.idEncargado);
 			SET errorMessage = CONCAT('Error: No se puede agregar el encargado ', CAST(NEW.idEncargado AS CHAR), ' a la sucursal ', CAST(NEW.idSucursal AS CHAR), ' porque no existe un empleado activo con ese id');
 			SIGNAL sqlstate '45000' SET message_text = errorMessage;
 		END IF;
 	END IF;
 end //
 
-insert into sucursales values(8, 454, 'P. Sherman Calle Wallaby', 300, null, 12345678);
-
-select * from sucursales;
-select * from personal;
-select * from inconsist_r3;
+delimiter //
+CREATE TRIGGER r3_encargados_AU AFTER UPDATE on sucursales
+for each row
+begin
+	DECLARE idPersonalExists int default(null);
+    DECLARE errorMessage varchar(255);
+    
+	IF NEW.idEncargado IS NOT NULL THEN
+		SELECT idPersonal INTO idPersonalExists
+        FROM personal
+        WHERE idPersonal = NEW.idEncargado
+			and activo = true;
+            
+		IF idPersonalExists IS NULL THEN
+			CALL insertInconsistencia(NEW.idSucursal, NEW.idEncargado);
+			SET errorMessage = CONCAT('Error: No se puede modificar el encargado ', CAST(NEW.idEncargado AS CHAR), ' en la sucursal ', CAST(NEW.idSucursal AS CHAR), ' porque no existe un empleado activo con ese id');
+			SIGNAL sqlstate '45000' SET message_text = errorMessage;
+		END IF;
+	END IF;
+end //
